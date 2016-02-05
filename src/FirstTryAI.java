e7c96f8b37c924cb74b84c69ae92269f1b60601f
FrancoisDemoullin <f.demoullin@gmail.com>
2016-02-04 21:15:57 -0800

    Addition of comments

--- src/FirstTryAI.java
+++ src/FirstTryAI.java
@@ -9,83 +9,99 @@ import java.util.*;
  * @author Francois Demoullin
  */
 
-class PointCost implements Comparable<PointCost>
-{
-    public Point mPoint;
-    public double mCost;
 
-    // constructor
-    public PointCost(final Point pPoint, final double pCost)
-    {
-        mPoint = pPoint;
-        mCost = pCost;
-    }
-
-    @Override
-    public int compareTo(PointCost pO1)
-    {
-        if (pO1.mCost - this.mCost > 0)
-        {
-            return -1;
-        }
-        if (pO1.mCost - this.mCost < 0 )
-        {
-            return 1;
-        }
-        return 0;
-    }
-}
 
 public class FirstTryAI implements AIModule
 {
+    // private class -> this would be a struct in a C like language
+    private class PointCost implements Comparable<PointCost>
+    {
+        // terrible coding practice, I know
+        public Point mPoint;
+        public double mCost;
+
+        // constructor
+        public PointCost(final Point pPoint, final double pCost)
+        {
+            mPoint = pPoint;
+            mCost = pCost;
+        }
+
+        // comparator is implemented to return the smallest of 2 elements
+        @Override
+        public int compareTo(PointCost pO1)
+        {
+            if (pO1.mCost - this.mCost > 0)
+            {
+                return -1;
+            }
+            if (pO1.mCost - this.mCost < 0 )
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
     // Creates the path to the goal using Dijkstras.
+
     public List<Point> createPath(final TerrainMap pMap)
     {
         // Holds the final path
-        ArrayList<Point> lPath = new ArrayList<Point>();
+        ArrayList<Point> lPath = new ArrayList<>();
 
+        // keeps track of all not yet expanded nodes
         PriorityQueue<PointCost> lUnsettled = new PriorityQueue<>();
+        // hashes a point to its SHORTEST distance from the root node
         Map<Point, Double> lDistance = new HashMap<>();
+        // hashes a node to its previous node
+        // the root is not hashed (root hashes to null)
         Map<Point, Point> lPrevious = new HashMap<>();
 
+        // add the root to both maps
         lDistance.put(pMap.getStartPoint(), 0D);
         lUnsettled.add(new PointCost(pMap.getStartPoint(), 0D));
 
+        // helpers for the while loop
         PointCost lCurrent = null;
         Point lCurrentPoint = null;
 
-        // here we go
         while(lUnsettled.size() != 0)
         {
+            // remove the most promising node from unsettled
             lCurrent = lUnsettled.poll();
             lCurrentPoint = lCurrent.mPoint;
 
             if (lCurrentPoint.x == pMap.getEndPoint().x && lCurrentPoint.y == pMap.getEndPoint().y)
             {
+                // we can break because the first solution we find has to be the best solution
                 break;
             }
-            // just make sure
-            assert(lCurrentPoint.x != pMap.getEndPoint().x || lCurrentPoint.y != pMap.getEndPoint().y);
 
             // get neighbors of the most promising node
             Point[] lLocalNeighbors = pMap.getNeighbors(lCurrentPoint);
 
             for (int i = 0; i < lLocalNeighbors.length; i++)
             {
+                // compute new cost
                 Double lCost = pMap.getCost(lCurrentPoint, lLocalNeighbors[i]) + lCurrent.mCost;
 
                 if (lDistance.get(lLocalNeighbors[i]) == null || lCost < lDistance.get(lLocalNeighbors[i]))
                 {
+                    // we either found an unvisited node or we found a better path to said node
                     lDistance.put(lLocalNeighbors[i], lCost);
                     PointCost lToBeExplored = new PointCost(lLocalNeighbors[i], lCost);
                     lPrevious.put(lLocalNeighbors[i], lCurrentPoint);
+
+                    // add it to unsettled, it is worth exploring this node
                     lUnsettled.add(lToBeExplored);
                 }
             }
         }
 
-        // add the goal node
+        // add the goal node first
         lPath.add(lCurrentPoint);
+
         // go back up the path and return
         Point lPrev = lPrevious.get(lCurrentPoint);
         while (lPrev != null)
@@ -95,5 +111,5 @@ public class FirstTryAI implements AIModule
         }
 
         return lPath;
-    }
+    } // memory from the maps is de-allocated here! No mem leaks possible
 }
