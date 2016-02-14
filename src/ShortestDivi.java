febc631f5fb95af2c6adb55f229fbaaf6a949948
FrancoisDemoullin <f.demoullin@gmail.com>
2016-02-14 13:38:02 -0800

    Divisible heurisitc

--- src/ShortestDivi.java
+++ src/ShortestDivi.java
@@ -1,12 +1,8 @@
-/**
- * Created by Francois on 2016-02-11.
- */
-
 import java.awt.Point;
 import java.util.*;
 
 
-public class FirstAStarTry implements AIModule
+public class ShortestDivi implements AIModule
 {
     // private class -> this would be a struct in a C like language
     private class PointCost implements Comparable<PointCost>
@@ -32,29 +28,68 @@ public class FirstAStarTry implements AIModule
 
     private double computeHeurisitc(final TerrainMap pMap, final Point pCurrentPoint)
     {
-        double lGoalHeight = pMap.getTile(pMap.getEndPoint());
+///*
+        double heuristic = 0.0;
+
+        double lGoalHeight = pMap.getTile(pMap.getEndPoint());  //getTile - HEIGHT OF TILE
         double lCurrentHeight = pMap.getTile(pCurrentPoint);
 
         double lDifferenceX = Math.abs(pMap.getEndPoint().x - pCurrentPoint.x);
         double lDifferenceY = Math.abs(pMap.getEndPoint().y - pCurrentPoint.y);
-        double lMasterDiff = Math.max(lDifferenceX, lDifferenceY);
-        double lHeightDifference = lCurrentHeight - lGoalHeight;
+        double lMasterDiff = Math.max(lDifferenceX, lDifferenceY);  //max heightdiff between x and y coord
+        double lMinDiff = Math.min(lDifferenceX, lDifferenceY);
+        double lHeightDifference = Math.abs(lCurrentHeight - lGoalHeight);   //always positive  - to match the divisive optimal path cost
 
-        if( lHeightDifference < 0 )
-        {
-            // we are above the endpoint
-            return (-lHeightDifference * Math.E) + lMasterDiff + lHeightDifference;
-        }
-        else if( lHeightDifference > 0 )
-        {
-            // we are below the endpoint
-            return (lHeightDifference / Math.E) + lMasterDiff - lHeightDifference;
-        }
-        else
-        {
-            // we are at the same height
-            return lMasterDiff;
-        }
+        double lDirectDistance = pCurrentPoint.distance(pMap.getEndPoint());
+
+        //return Math.sqrt(lHeightDifference/Math.E) + lMasterDiff ;
+
+        //return (lHeightDifference / Math.E) + lMasterDiff - lHeightDifference; - original
+
+        //lHeightDifference / (0.5*Math.E) -- PathCost, 552.5487948977717, Uncovered, 83728, TimeTaken, 105720
+        //                                 -- PathCost, 198.59165501141644, Uncovered, 2115, TimeTaken, 69
+        //( lMinDiff / lHeightDifference) + lMasterDiff - lHeightDifference -- PathCost, 250 - divisive
+        //lHeightDifference / (2*Math.E) -- PathCost, 552.5142702459052, Uncovered, 125396, TimeTaken, 123610
+        //lHeightDifference / (lMasterDiff)  -- PathCost, 552.5116374761492, Uncovered, 137652, TimeTaken, 145225
+        // (lHeightDifference / Math.E) + lMasterDiff -- PathCost, 552.9955885491745, Uncovered, 15812, TimeTaken, 1581
+        //Math.abs( (Math.sqrt(lHeightDifference)) -- PathCost, 552.5265822768905, Uncovered, 131076, TimeTaken, 125831
+        //(Math.sqrt(lHeightDifference)) + lMasterDiff -- PathCost, 553.9812790048047, Uncovered, 10219, TimeTaken, 1081
+        //Math.sqrt(lHeightDifference/Math.E) + lMasterDiff -- PathCost, 552.9986846666419, Uncovered, 6092, TimeTaken, 545
+        //                                                  -- PathCost, 198.59165501141644, Uncovered, 1407, TimeTaken, 31
+        //Math.sqrt(lHeightDifference) + lMasterDiff ; -- PathCost, 553.9812790048047, Uncovered, 10219, TimeTaken, 984
+        //                                             -- PathCost, 198.59165501141644, Uncovered, 1753, TimeTaken, 38
+/*
+double p = 1/lMasterDiff; // p <(minimum cost of taking one step)/(expected maximum path length)
+heuristic *= (1.0 + p);
+return heuristic;
+*/
+/*
+double dx1 = lDifferenceX;
+double dy1 = lDifferenceY;
+double dx2 = start.x - goal.0;
+double dy2 = start.y - goal.y;
+cross = abs(dx1*dy2 - dx2*dy1)
+double heuristic = 0;
+heuristic += cross*0.001;
+return (heuristic);
+*/
+/*
+double D2 = Math.sqrt(2);
+double D = lMasterDiff * Math.E;
+double dx = lDifferenceX;
+double dy = lDifferenceY;
+return D * (dx + dy) + (D2 - 2 * D) * Math.min(dx, dy);
+*/
+///*
+//Euclidean
+        double D = 1; //D to the lowest cost between adjacent squares
+        double dx = lDifferenceX;
+        double dy = lDifferenceY;
+        return D * Math.sqrt(dx * dx + dy * dy);
+//PathCost, 198.59165501141644, Uncovered, 1004, TimeTaken, 23
+//PathCost, 550.7157755911452, Uncovered, 2467, TimeTaken, 15
+//*/
+
     }
 
     // Creates the path to the goal using a heurisitc.
@@ -81,7 +116,7 @@ public class FirstAStarTry implements AIModule
         Point lCurrentPoint = null;
 
 
-        while(lUnsettled.size() != 0)
+        while(!lUnsettled.isEmpty()) //(lUnsettled.size() != 0)
         {
             // remove the most promising node from unsettled
             lCurrent = lUnsettled.poll();
